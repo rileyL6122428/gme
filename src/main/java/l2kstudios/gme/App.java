d698e54aac0164f4e510147664dd4d12abea9a11
Riley Littlefield <rileylittlefield@ymail.com>
2017-04-04 23:34:12 -0400

    sets up hello world for processing sketch and spring

--- src/main/java/l2kstudios/gme/App.java
+++ src/main/java/l2kstudios/gme/App.java
@@ -1,10 +1,14 @@
 package l2kstudios.gme;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import processing.core.PApplet;
 
 public class App extends PApplet {
     public static void main( String[] args ) {
-    	PApplet papplet = new App();
+    	ApplicationContext appContext = new ClassPathXmlApplicationContext("Beans.xml");
+    	PApplet papplet = (PApplet) appContext.getBean("app");
     	papplet.runSketch(new String[]{ "PraxisLIVE" }, papplet);
     }
     
