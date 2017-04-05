2fb2af6f6711dd4feb1ae6d99fe36bec1dd23326
Riley Littlefield <rileylittlefield@ymail.com>
2017-04-04 23:25:48 -0400

    initial commit

---
+++ src/main/java/l2kstudios/gme/App.java
@@ -0,0 +1,22 @@
+package l2kstudios.gme;
+
+import processing.core.PApplet;
+
+public class App extends PApplet {
+    public static void main( String[] args ) {
+    	PApplet papplet = new App();
+    	papplet.runSketch(new String[]{ "PraxisLIVE" }, papplet);
+    }
+    
+    public void settings() {
+    	
+    }
+    
+    public void setup() {
+    	
+    }
+    
+    public void draw() {
+    	
+    }
+}
