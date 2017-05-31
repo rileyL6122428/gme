e22f822f945fc765d95b95e75443139b8c9ee1b4
Riley Littlefield <rileylittlefield@ymail.com>
2017-05-31 08:42:30 -0400

    deletes extraneous dependencies

--- src/test/java/l2kstudios/gme/model/action/rangeofeffect/RangeOfEffectTest.java
+++ src/test/java/l2kstudios/gme/model/action/rangeofeffect/RangeOfEffectTest.java
@@ -1,17 +1,14 @@
 package l2kstudios.gme.model.action.rangeofeffect;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 import l2kstudios.gme.model.grid.PlayingGrid;
