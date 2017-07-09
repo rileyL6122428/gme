1e4438c3487ac774e90dd9a7d35110341599d511
Riley Littlefield <rileylittlefield@ymail.com>
2017-07-09 18:03:53 -0400

    adds rotation methods for delta

--- src/main/java/l2kstudios/gme/model/action/rangeofeffect/Delta.java
+++ src/main/java/l2kstudios/gme/model/action/rangeofeffect/Delta.java
@@ -1,7 +1,12 @@
 package l2kstudios.gme.model.action.rangeofeffect;
 
+import static java.lang.Math.atan2;
+import static java.lang.Math.cos;
+import static java.lang.Math.round;
+import static java.lang.Math.sin;
+import static java.lang.Math.toRadians;
+
 import l2kstudios.gme.model.grid.Position;
-import l2kstudios.gme.model.grid.Space;
 
 public class Delta {
 	
@@ -21,9 +26,25 @@ public class Delta {
 	public int getX() {
 		return x;
 	}
-	
+		
 	public int getY() {
 		return y;
+	}
+	
+	public int getRotatedX(double rotationDegrees) {
+		return (int) round(cos(rotationTheta(rotationDegrees)) * length()) ;
+	}
+	
+	public int getRotatedY(double rotationDegrees) {
+		return (int) -round(sin(rotationTheta(rotationDegrees)) * length());
+	}
+	
+	private double rotationTheta(double rotationDegrees) {
+		return toRadians(rotationDegrees) - atan2(y, x);
+	}
+	
+	private double length() {
+		return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
 	}
 	
 	public boolean isZeroDelta() {
--- src/test/java/l2kstudios/gme/model/action/rangeofeffect/DeltaTest.java
+++ src/test/java/l2kstudios/gme/model/action/rangeofeffect/DeltaTest.java
@@ -2,12 +2,14 @@ package l2kstudios.gme.model.action.rangeofeffect;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+
+import org.junit.Ignore;
 import org.junit.Test;
 
 import l2kstudios.gme.model.grid.Position;
 
 public class DeltaTest {
-	
 
 	@Test
 	public void constructor_providedTwoPositions_xAndYAreDifferenceOfPositionCoords() {
@@ -19,5 +21,142 @@ public class DeltaTest {
 		assertEquals(1, delta.getX());
 		assertEquals(0, delta.getY());
 	}
-
+	
+	static class RotationTestData {
+		
+		private double rotationDegrees;
+		
+		private int deltaX;
+		private int deltaY;
+		
+		private int rotatedDeltaX;
+		private int rotatedDeltaY;
+		
+		public double getRotation() {
+			return rotationDegrees;
+		}
+		public void setRotation(double rotation) {
+			this.rotationDegrees = rotation;
+		}
+		public int getDeltaX() {
+			return deltaX;
+		}
+		public void setDeltaX(int deltaX) {
+			this.deltaX = deltaX;
+		}
+		public int getDeltaY() {
+			return deltaY;
+		}
+		public void setDeltaY(int deltaY) {
+			this.deltaY = deltaY;
+		}
+		public int getRotatedDeltaX() {
+			return rotatedDeltaX;
+		}
+		public void setRotatedDeltaX(int rotatedDeltaX) {
+			this.rotatedDeltaX = rotatedDeltaX;
+		}
+		public int getRotatedDeltaY() {
+			return rotatedDeltaY;
+		}
+		public void setRotatedDeltaY(int rotatedDeltaY) {
+			this.rotatedDeltaY = rotatedDeltaY;
+		}
+	}
+	
+	@Test
+	public void getRotatedCoordinate_0DegreeRotation_returnsDeltaCoordinate() {
+		new ArrayList<RotationTestData>(){{
+			add(new RotationTestData(){{
+				setDeltaX(0);
+				setDeltaY(0);
+				setRotation(0);
+				setRotatedDeltaX(0);
+				setRotatedDeltaY(0);
+			}});
+			
+			add(new RotationTestData(){{
+				setDeltaX(2);
+				setDeltaY(3);
+				setRotation(0);
+				setRotatedDeltaX(2);
+				setRotatedDeltaY(3);
+			}});
+			
+		}}.forEach(this::verifyRotationReturnsCorrectDeltaCoordinates);
+	}
+	
+	@Test
+	public void getRotatedCoordinate_90DegreeRotation_returnsTransformedDeltaCoordinate() {
+		new ArrayList<RotationTestData>(){{
+			add(new RotationTestData(){{
+				setDeltaX(0);
+				setDeltaY(0);
+				setRotation(90);
+				setRotatedDeltaX(0);
+				setRotatedDeltaY(0);
+			}});
+			
+			add(new RotationTestData(){{
+				setDeltaX(2);
+				setDeltaY(3);
+				setRotation(90);
+				setRotatedDeltaX(3);
+				setRotatedDeltaY(-2);
+			}});
+			
+		}}.forEach(this::verifyRotationReturnsCorrectDeltaCoordinates);
+	}
+	
+	@Test
+	public void getRotatedCoordinate_180DegreeRotation_returnsTransformedDeltaCoordinate() {
+		new ArrayList<RotationTestData>(){{
+			add(new RotationTestData(){{
+				setDeltaX(0);
+				setDeltaY(0);
+				setRotation(180);
+				setRotatedDeltaX(0);
+				setRotatedDeltaY(0);
+			}});
+			
+			add(new RotationTestData(){{
+				setDeltaX(2);
+				setDeltaY(3);
+				setRotation(180);
+				setRotatedDeltaX(-2);
+				setRotatedDeltaY(-3);
+			}});
+			
+		}}.forEach(this::verifyRotationReturnsCorrectDeltaCoordinates);
+	}
+	
+	@Test
+	public void getRotatedCoordinate_270DegreeRotation_returnsTransformedDeltaCoordinate() {
+		new ArrayList<RotationTestData>(){{
+			add(new RotationTestData(){{
+				setDeltaX(0);
+				setDeltaY(0);
+				setRotation(270);
+				setRotatedDeltaX(0);
+				setRotatedDeltaY(0);
+			}});
+			
+			add(new RotationTestData(){{
+				setDeltaX(2);
+				setDeltaY(3);
+				setRotation(270);
+				setRotatedDeltaX(-3);
+				setRotatedDeltaY(2);
+			}});
+			
+		}}.forEach(this::verifyRotationReturnsCorrectDeltaCoordinates);
+	}
+	
+	
+	
+	private void verifyRotationReturnsCorrectDeltaCoordinates(RotationTestData testData) {
+		Delta delta = new Delta(testData.getDeltaX(), testData.getDeltaY());
+		assertEquals(testData.getRotatedDeltaX(), delta.getRotatedX(testData.getRotation()));
+		assertEquals(testData.getRotatedDeltaY(), delta.getRotatedY(testData.getRotation()));
+	}
 }
