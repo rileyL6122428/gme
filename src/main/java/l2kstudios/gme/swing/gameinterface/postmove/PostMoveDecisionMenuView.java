8cca47efd0d9375b4463a9cde5afa2c606139814
Riley Littlefield <rileylittlefield@ymail.com>
2017-08-06 00:05:57 -0400

    refactors view for PostMoveDecisionMenu

--- src/main/java/l2kstudios/gme/swing/gameinterface/postmove/PostMoveDecisionMenuView.java
+++ src/main/java/l2kstudios/gme/swing/gameinterface/postmove/PostMoveDecisionMenuView.java
@@ -1,14 +1,14 @@
 package l2kstudios.gme.swing.gameinterface.postmove;
 
+import static java.awt.Color.BLACK;
+import static java.awt.Color.GREEN;
 import static l2kstudios.gme.swing.view.GridConstants.SPACE_HEIGHT;
 import static l2kstudios.gme.swing.view.GridConstants.SPACE_WIDTH;
 
-import java.awt.Color;
 import java.awt.Graphics;
 import java.util.List;
 
 import l2kstudios.gme.model.action.postmove.PostMoveDecision;
-import l2kstudios.gme.model.grid.Cursor;
 import l2kstudios.gme.model.grid.Position;
 import l2kstudios.gme.model.turn.Turn;
 import l2kstudios.gme.model.unit.Unit;
@@ -24,35 +24,39 @@ public class PostMoveDecisionMenuView implements View {
 
 	@Override
 	public void draw(Graphics drawingCtx) {
-		drawDecisionName(drawingCtx);
+		drawDecisionNames(drawingCtx);
 	}
 	
-	private void drawDecisionName(Graphics drawingCtx) {
+	private void drawDecisionNames(Graphics drawingCtx) {
 		List<PostMoveDecision> decisions = postMoveDecisionMenu.getDecisons();
-		Unit actingUnit = postMoveDecisionMenu.getActingUnit();
 		
 		for(int placementIdx = 0; placementIdx < decisions.size(); placementIdx++) {
 			int decisionIdx = decisions.size() - (placementIdx + 1);
 			PostMoveDecision decision = decisions.get(decisionIdx);
-			Position unitPosition = actingUnit.getPosition();
 			
 			setDecisionColor(decisionIdx, drawingCtx);
-			
-			drawingCtx.drawString(
-				decision.getName(), 
-				SPACE_WIDTH * unitPosition.getX() + 50,
-				SPACE_HEIGHT * unitPosition.getY() - 10 + decisionIdx * 25
-			);
+			drawActionTypeName(decision, decisionIdx, drawingCtx);
 		}
 		
 	}
 	
 	private void setDecisionColor(int decisionIdx, Graphics drawingCtx) {
 		if(decisionIdx == postMoveDecisionMenu.getCursorY()) {
-			drawingCtx.setColor(Color.GREEN);
+			drawingCtx.setColor(GREEN);
 		} else {
-			drawingCtx.setColor(Color.BLACK);
+			drawingCtx.setColor(BLACK);
 		}
+	}
+	
+	private void drawActionTypeName(PostMoveDecision decision, int decisionIdx, Graphics drawingCtx) {
+		Unit actingUnit = postMoveDecisionMenu.getActingUnit();
+		Position unitPosition = actingUnit.getPosition();
+		
+		drawingCtx.drawString(
+			decision.getName(), 
+			SPACE_WIDTH * unitPosition.getX() + 50,
+			SPACE_HEIGHT * unitPosition.getY() - 10 + decisionIdx * 25
+		);
 	}
 
 }
