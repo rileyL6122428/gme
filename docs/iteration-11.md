3f80bc501e09711afad2854b65f6e7f00d8d223f
Riley Littlefield <rileylittlefield@ymail.com>
2017-08-16 23:24:08 -0400

    adds basic health bar

---
+++ docs/iteration-11.md
@@ -0,0 +1,13 @@
+## Iteration 11
+
+	* add health bars
+	* fix scroll bug
+	* clean up unit tests
+	* Damage and health recovery denotations
+	
+	
+	- steps to recreate scroll bug
+		with asbel, 
+			choose sword artes
+			try to place sword arte on asbel
+			backspace out 
--- src/main/java/l2kstudios/gme/swing/view/CharacterView.java
+++ src/main/java/l2kstudios/gme/swing/view/CharacterView.java
@@ -1,9 +1,14 @@
 package l2kstudios.gme.swing.view;
 
+import java.awt.Color;
 import java.awt.Graphics;
 
+import l2kstudios.gme.model.grid.Position;
 import l2kstudios.gme.model.unit.Unit;
 import l2kstudios.gme.swing.modelwrappers.SwingUnit; 
+
+import static l2kstudios.gme.model.unit.Unit.StatType.*;
+import static l2kstudios.gme.swing.view.GridConstants.*;
 
 public class CharacterView {
 	
@@ -11,6 +16,24 @@ public class CharacterView {
 	
 	public void draw(Graphics drawingCtx) {
 		unit.runBoardAnimation(drawingCtx);
+		drawHealthBar(drawingCtx);
+	}
+
+	private void drawHealthBar(Graphics drawingCtx) {
+		Position position = unit.getPosition();
+		
+		drawingCtx.setColor(Color.GREEN);
+		
+		drawingCtx.drawLine(
+			(position.getX() + 1) * SPACE_WIDTH, 
+			(position.getY() + 1) * SPACE_HEIGHT, 
+			(position.getX() + 1) * SPACE_WIDTH, 
+			(int)((position.getY() + 1 - unitHealthRatio()) * SPACE_HEIGHT)
+		);
+	}
+	
+	private float unitHealthRatio() {
+		return ((float)unit.get(HEALTH)) / ((float)unit.getMax(HEALTH));
 	}
 
 	public Unit getUnit() {
